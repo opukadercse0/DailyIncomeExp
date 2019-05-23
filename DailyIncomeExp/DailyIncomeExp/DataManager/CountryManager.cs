e7060196c58f179f6665184e30c8bfaf2a8cf3af
opukadercse0 <opukadercse0@gmail.com>
2019-05-23 14:14:13 -0700

    ContryManager Class added and In CountryManager Loadcountry Method added for Loading Country From Database

---
+++ DailyIncomeExp/DailyIncomeExp/DataManager/CountryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DailyIncomeExp.DataAccess;
+using DailyIncomeExp.Models;
+
+namespace DailyIncomeExp.DataManager
+{
+    public class CountryManager
+    {
+        public List<CountryModel> LoadCountry()
+        {
+            string sql = "Select * From Country";
+            return SqlDataAccess.LoadData<CountryModel>(sql);
+        }
+    }
+}
