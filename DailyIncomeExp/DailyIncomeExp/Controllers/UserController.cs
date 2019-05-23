bbb0cbf1eb898160c11b44bee2a516ea9b05536f
opukadercse0 <opukadercse0@gmail.com>
2019-05-23 13:05:15 -0700

    added a controler UserControler, create a Registration View, and added Registration layout page

---
+++ DailyIncomeExp/DailyIncomeExp/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DailyIncomeExp.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        //Login
+       
+    }
+}
