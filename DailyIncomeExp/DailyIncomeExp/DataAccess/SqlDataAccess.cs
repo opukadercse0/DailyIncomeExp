efb256c761deaec5950bc06445540dd7703bbf63
opukadercse0 <opukadercse0@gmail.com>
2019-05-23 13:59:47 -0700

    Create a SqlDataAccessClass, setting Database  ConnectionString, added SaveData and for loading Country Data added LoadData Method

---
+++ DailyIncomeExp/DailyIncomeExp/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace DailyIncomeExp.DataAccess
+{
+    public class SqlDataAccess
+    {
+        //IDBconnection -===Represents an open connection to a data source,
+        //and is implemented by .NET Framework data providers
+        //that access relational databases.
+        public static string GetConnectionString(string connectionName = "DailyExpInDB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+        //for saving data into database
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                  return  cnn.Execute(sql, data);
+            }
+        }
+        //for loading data
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn=new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+    }
+}
