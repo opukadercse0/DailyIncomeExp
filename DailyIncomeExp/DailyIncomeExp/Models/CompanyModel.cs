35165e72206fed92ef958de0ba3a22648c04155c
opukadercse0 <opukadercse0@gmail.com>
2019-05-23 12:46:26 -0700

    Create View_Model Folder in Model , added CountryModel& ViewModel Class in Model and added a CompanyViewModel in ViewModel folder

---
+++ DailyIncomeExp/DailyIncomeExp/Models/CompanyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyIncomeExp.Models
+{
+    public class CompanyModel
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string CompaniEmail { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int CountryId { get; set; }
+
+    }
+}
---
+++ DailyIncomeExp/DailyIncomeExp/Models/ViewModel/CompanyViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DailyIncomeExp.Models.ViewModel
+{
+    public class CompanyViewModel
+    {
+        [Display(Name = "CompanyName")]
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "CompaniEmail")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string CompaniEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage = "Insert at least 6 character password")]
+        public string Password { get; set; }
+       
+        [Display(Name = "AgainPassword")]
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match to the password")]
+        public string AgainPassword { get; set; }
+
+        [Display(Name = "Address")]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "CompanyId")]
+        [Required]
+        public int CountryId { get; set; }
+    }
+}
